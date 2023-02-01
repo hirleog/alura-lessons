c8596ccc0e404d30a83c56bd29a317ac27a23189
hirleog <87393381+hirleog@users.noreply.github.com>
2023-01-31 21:54:35 -0300

    fist commit

---
+++ OneDrive/Documentos/Angular/alura-lessons/rxjs/2685-angular-rxjs-projeto-base/src/app/views/lista-livros/lista-livros.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-livros',
+  templateUrl: './lista-livros.component.html',
+  styleUrls: ['./lista-livros.component.css']
+})
+export class ListaLivrosComponent {
+
+  listaLivros: [];
+
+  constructor() { }
+
+}
+
+
+
